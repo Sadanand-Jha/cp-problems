a6334bb41597fc60778abda56bf419d4196a0153
Sadanand_Jha <coder.sadanand.dseu@gmail.com>
2026-07-02 13:34:29 +0530

    first commit

---
+++ get_an_empty_string2.cpp
@@ -0,0 +1,132 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+typedef long long ll;
+typedef vector<long long> vll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<char> > vvc;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+typedef stack<int> sti;
+typedef queue<int> qi;
+typedef vector<pair<long, long> > vpll;
+
+
+#define endl "\n"
+#define Ft front
+#define Bk back
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define rep(i,a,b) for (int i = a; i <= b; i++)
+
+const ll MOD = 1e9 + 7;
+
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+// void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+
+
+
+
+void solve() {
+    int count = 0;
+    string s;
+    cin>> s;
+    int n = s.length();
+
+    vi lastIdx(26, -1);
+    vi dp(n);
+
+    for(int i = 0; i < n; i++) {
+        if(lastIdx[s[i] - 'a'] == -1) {
+            //firsttime;
+            if(i == 0) {
+                dp[i] = 1;
+            }
+            else{
+                dp[i] = dp[i - 1] + 1;
+            }
+            lastIdx[s[i] - 'a'] = i;
+        }
+        else{
+            // remove or sustains
+            int cost = i - lastIdx[s[i] - 'a'] - 1;
+            dp[i] = min(dp[i - 1] + 1, (cost + ((lastIdx[s[i] - 'a'] - 1 >= 0 ) ? dp[lastIdx[s[i] - 'a'] - 1]: 0)));
+            lastIdx[s[i] - 'a'] = i;
+        }
+    }
+
+    cout<< dp.back()<< endl;
+
+}
+
+
+
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("debug.txt", "w", stderr);
+#endif
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
