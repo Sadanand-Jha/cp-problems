6180dc50811a090d59a7ed219276ea0d3d4aca74
Sadanand_Jha <coder.sadanand.dseu@gmail.com>
2026-07-02 17:04:34 +0530

    three problem today

---
+++ increase_subarray_sums.cpp
@@ -0,0 +1,151 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+typedef long long ll;
+typedef vector<long long> vll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<char> > vvc;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+typedef stack<int> sti;
+typedef queue<int> qi;
+typedef vector<pair<long, long> > vpll;
+ 
+ 
+#define endl "\n"
+#define Ft front
+#define Bk back
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define rep(i,a,b) for (int i = a; i <= b; i++)
+ 
+const ll MOD = 1e9 + 7;
+ 
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(ll t) {cerr << t;}
+// void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+ 
+ 
+ 
+ 
+ 
+void solve() {
+    int count = 0;
+    int n;
+    cin>>n;
+    int x;
+    cin>> x;
+    vi vec(n);
+    for(int i = 0; i  < n; i++){
+        cin>>vec[i];
+    }
+    vi maxSum(n + 1);
+
+    for(int i = 1; i <= n ; i++) {
+        int sum = 0;
+        int maxi = LLONG_MIN;
+        for(int j = 0; j < i; j++) {
+            sum += vec[j];
+        }
+
+        maxi = max(maxi, sum);
+
+        int removeIdx = 0;
+        for(int j = i; j < n; j++) {
+            sum += vec[j];
+            sum -= vec[removeIdx++];
+            maxi = max(maxi, sum);
+        }
+        maxSum[i] = maxi;
+    }
+
+
+    vi answer(n + 1);
+    int maxVal = LLONG_MIN;
+    for(int i = n; i >= 0; i--) {
+        maxVal = max(maxSum[i], maxVal);
+        answer[i] = maxVal + i * x;
+    }
+
+    maxVal = LLONG_MIN;
+
+    for(int i = 0; i <= n; i++) {
+        maxVal = max(maxVal, maxSum[i] + (i * x));
+        answer[i] = max(answer[i], maxVal);   
+    }
+    for(auto it: answer) {
+        cout<< it<< ' ';
+    }
+    cout<< endl;
+ 
+}
+ 
+ 
+ 
+ 
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("debug.txt", "w", stderr);
+#endif
+ 
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
